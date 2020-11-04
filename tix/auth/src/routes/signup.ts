0b9495e24369adafc25968aed9db7941046b3dd5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-04 09:15:54 -0500

    add express validator and validate email/password

--- tix/auth/src/routes/signup.ts
+++ tix/auth/src/routes/signup.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('hello');
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters')
+],
+(req, res) => {
+  const { email, password } = req.body;
 });
 
 export { router as signupRouter };
