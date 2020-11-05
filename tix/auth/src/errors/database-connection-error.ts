ad07e2f6d8e4c46d0b76bead92ba0450ff5d3e3e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-05 15:53:24 -0500

    invert errors logic away from middleware to classes

--- tix/auth/src/errors/database-connection-error.ts
+++ tix/auth/src/errors/database-connection-error.ts
@@ -1,10 +1,17 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = 'Error connecting to database';
-  
+
   constructor() {
     super();
 
     // only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: this.reason }
+    ];
   }
 }
