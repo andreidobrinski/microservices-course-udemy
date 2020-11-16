de228e40e37aed4e901f398e8e8bad49cd224447
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-15 23:23:37 -0500

    test cookie set on header

--- tix/auth/src/routes/__test__/signup.test.ts
+++ tix/auth/src/routes/__test__/signup.test.ts
@@ -55,7 +55,7 @@ it('disallows duplicate emails', async () => {
       password: 'password'
     })
     .expect(201);
-    
+
   await request(app)
     .post('/api/users/signup')
     .send({
@@ -63,4 +63,16 @@ it('disallows duplicate emails', async () => {
       password: 'password'
     })
     .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+  
+  expect(response.get('Set-Cookie')).toBeDefined();
 });
