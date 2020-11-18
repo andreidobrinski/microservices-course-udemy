7b78ee43faf2a0407cc9c246aef5620f0a640eb9
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-17 19:45:47 -0500

    add error handling to signup

--- tix/client/pages/auth/signup.js
+++ tix/client/pages/auth/signup.js
@@ -4,13 +4,18 @@ import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post('/api/users/signup', {
-      email, password
-    });
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -33,6 +38,18 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul>
+            {errors.map(err => (
+              <li key={err.message}>
+                {err.message}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
