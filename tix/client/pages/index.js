8452b7012ccc492417aac28487b98475bd9ed4a7
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-19 10:58:04 -0500

    add getInitialProps to landing page

--- tix/client/pages/index.js
+++ tix/client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
   return <h1>landing page</h1>
 };
+
+LandingPage.getInitialProps = () => {
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
