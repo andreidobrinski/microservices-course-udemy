5d0e7b2d8edfd5e3d7da89f0bb0476f6edc21086
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-26 22:34:50 -0500

    add happy path for updating a ticket

--- tix/tickets/src/routes/__test__/update.test.ts
+++ tix/tickets/src/routes/__test__/update.test.ts
@@ -74,5 +74,28 @@ it('returns a 400 if the user provides an invalid title or price', async () =>{
 });
 
 it('updates the ticket provided valid inputs', async () =>{
+  const cookie = global.signin();
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'title',
+      price: 20
+    });
 
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'title2',
+      price: 10
+    })
+    .expect(200);
+  
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('title2');
+  expect(ticketResponse.body.price).toEqual(10);
 });
--- tix/tickets/src/routes/update.ts
+++ tix/tickets/src/routes/update.ts
@@ -32,6 +32,12 @@ async (req: Request, res: Response) => {
     throw new NotAuthorizedError();
   }
 
+  ticket.set({
+    title: req.body.title,
+    price: req.body.price
+  });
+  await ticket.save();
+
   res.send(ticket);
 });
 
