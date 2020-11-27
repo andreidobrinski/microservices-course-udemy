b17913242282eb72887db79a10921ba650439aef
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-11-26 22:31:48 -0500

    validate email and price on ticket update

--- tix/tickets/src/routes/update.ts
+++ tix/tickets/src/routes/update.ts
@@ -10,7 +10,18 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+router.put('/api/tickets/:id', requireAuth,
+[
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0'),
+],
+validateRequest,
+async (req: Request, res: Response) => {
   const ticket = await Ticket.findById(req.params.id);
 
   if (!ticket) {
