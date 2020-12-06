53353ae5bf6da9744c53959d78a917fbbfacff65
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-06 11:58:46 -0500

    add subclass implementation

--- tix/nats-test/src/listener.ts
+++ tix/nats-test/src/listener.ts
@@ -7,6 +7,39 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
+// old test implementation
+// stan.on('connect', () => {
+//   console.log('Listener connected to nats');
+
+//   stan.on('close', () => {
+//     console.log('NATS connection closed');
+//     process.exit();
+//   });
+
+//   const options = stan
+//     .subscriptionOptions()
+//     .setManualAckMode(true)
+//     .setDeliverAllAvailable()
+//     .setDurableName('accounting-service');
+  
+//   const subscription = stan.subscribe(
+//     'ticket:created',
+//     'orders-service-queue-group',
+//     options
+//   );
+
+//   subscription.on('message', (msg: Message) => {
+//     const data = msg.getData();
+
+//     if (typeof data === 'string') {
+//       console.log(`Received event ${msg.getSequence()}, with data: ${data}`)
+//     }
+
+//     msg.ack();
+//   });
+// });
+
+// subclass implementation
 stan.on('connect', () => {
   console.log('Listener connected to nats');
 
@@ -15,27 +48,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-  
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event ${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
