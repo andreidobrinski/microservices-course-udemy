c235a86a21ee35e1ea430e86a8e639a3e280f383
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-06 11:29:40 -0500

    add durable subscriptions

--- tix/nats-test/src/listener.ts
+++ tix/nats-test/src/listener.ts
@@ -17,7 +17,9 @@ stan.on('connect', () => {
 
   const options = stan
     .subscriptionOptions()
-    .setManualAckMode(true);
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
   
   const subscription = stan.subscribe(
     'ticket:created',
