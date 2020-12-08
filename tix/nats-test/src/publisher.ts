c65899564259c5ee60f6986c30c4606f92599a4b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-07 21:37:56 -0500

    add publisher test implementation

--- tix/nats-test/src/publisher.ts
+++ tix/nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -6,18 +7,29 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
-  console.log('Publisher connected to NATS');
+// test implementation
 
-  const data = {
+// stan.on('connect', () => {
+//   console.log('Publisher connected to NATS');
+
+//   const data = {
+//     id: '123',
+//     title: 'concert',
+//     price: 20
+//   };
+
+//   const jsonData = JSON.stringify(data);
+
+//   stan.publish('ticket:created', jsonData, () => {
+//     console.log('Event published');
+//   });
+// });
+
+stan.on('connect', () => {
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: '123',
     title: 'concert',
     price: 20
-  };
-
-  const jsonData = JSON.stringify(data);
-
-  stan.publish('ticket:created', jsonData, () => {
-    console.log('Event published');
   });
 });
