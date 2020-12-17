5766e1ee502612d2726bedf22094bec71dbe707e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-17 16:51:23 -0500

    start on new order logic

--- tix/orders/src/routes/new.ts
+++ tix/orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@course-learning-ad/common';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@course-learning-ad/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -17,6 +19,36 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // find the ticket that the user is trying to order in the db
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // make sure that this ticket is not already reserved
+    // run query to look at all orders, find order where the ticket is the ticket we found
+    // if we find an order, that means the ticket is reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in:[
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete
+        ]
+      }
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // calculate an expiration date for this order
+
+    // build the order and save it to the db
+
+    // publish an event saying that an order was created
     res.send({});
   }
 );
