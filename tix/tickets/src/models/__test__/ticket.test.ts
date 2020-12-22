4a379e121cb115baca4bb212bdea059c019675cf
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-22 14:37:26 -0500

    test optimistic concurrency control

---
+++ tix/tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,39 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async (done) => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123'
+  });
+
+  // save the ticket to the db
+  await ticket.save()
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two separate changes to the tickets we fetch
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+  await firstInstance!.save();
+
+  // save the second fetched ticket and expect an error
+  
+  // works but has interop issues with jest and TS
+  expect(async () => {
+    await secondInstance!.save();
+  }).toThrow();
+
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error('should not reach this point');
+});
