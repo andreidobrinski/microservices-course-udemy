3f1f371afa3d054ccd456ee7a3dc2ac0cfd3ef22
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-22 17:59:21 -0500

    outline tests for listeners

---
+++ tix/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,19 @@
+const setup = async () => {
+  // create an instance of the listener
+
+  // create a fake data event
+
+  // create a fake message object
+};
+
+it('creates and saves a ticket', async () => {
+  // call the onMessage fn with the data + msg object
+
+  // expect a ticket was created
+});
+
+it('acks the message', async () => {
+  // call the onMessage fn with the data + msg object
+
+  // expect ack to be called
+});
