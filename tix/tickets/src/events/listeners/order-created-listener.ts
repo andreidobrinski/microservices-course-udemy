1207f9e652a946c16945d1af7260ccbddf900b9f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-23 15:34:55 -0500

    set order id on reserved ticket

--- tix/tickets/src/events/listeners/order-created-listener.ts
+++ tix/tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@course-learning-ad/common';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -8,6 +9,21 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // if no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
   }
 }
