6cd59e37c01c7470ae98ea55bca5f16ef6d59006
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-12-30 10:56:54 -0500

    add stripe charge

--- tix/payments/src/routes/new.ts
+++ tix/payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus
 } from '@course-learning-ad/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -40,6 +41,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token
+    });
   }
 );
 
